f275fc0c759017c6c1cefe5af5f275aca13c0721
AxisCodee <raneemma934@gmail.com>
2024-05-30 12:25:50 +0300

    transaction

--- app/Services/StoreArticleService.php
+++ app/Services/StoreArticleService.php
@@ -23,40 +23,39 @@ class StoreArticleService
 
     public function guardian()
     {
-
-        $date = '2024-01-01';
+        $date = Carbon::now()->format('Y-m-d');
         $subCategories = SubCategory::get();
 
-        foreach ($subCategories as $subCategory) {
-            $category = Category::findOrFail($subCategory->category_id);
+        DB::transaction(function () use ($subCategories, $date) {
+            foreach ($subCategories as $subCategory) {
+                $category = Category::findOrFail($subCategory->category_id);
 
-            $guardianResponse = app(ArticleService::class)->fetchNewsArticles('https://content.guardianapis.com/search', $subCategory->name, $date, '35e4f400-678d-4d12-bf63-b7e75091d583', $category->name . '/' . $category->name);
-            if ($guardianResponse->successful()) {
+                $guardianResponse = app(ArticleService::class)->fetchNewsArticles('https://content.guardianapis.com/search', $subCategory->name, $date, '35e4f400-678d-4d12-bf63-b7e75091d583', $category->name . '/' . $category->name);
+                if ($guardianResponse->successful()) {
+                    $lastDate = DB::table('articles')
+                        ->whereNotNull('article_id')
+                        ->max('date');
 
+                    $articles = [];
+                    foreach ($guardianResponse['response']['results'] as $result) {
+                        if (Carbon::parse($result['webPublicationDate'])->format('Y-m-d H:i:s') >= $lastDate) {
+                            $articles[] = [
+                                'title' => $result['webTitle'],
+                                'sub_category_id' => $subCategory->id,
+                                'article_id' => $result['id'],
+                                'date' => Carbon::parse($result['webPublicationDate'])->format('Y-m-d H:i:s'),
+                                'content' => $result['webUrl'],
+                                'source_id' => null,
+                            ];
+                        }
+                    }
 
-                $lastDate = DB::table('articles')
-                    ->whereNotNull('article_id')
-                    ->max('date');
-
-                $articles = [];
-                foreach ($guardianResponse['response']['results'] as $result) {
-                    if (Carbon::parse($result['webPublicationDate'])->format('Y-m-d H:i:s') >= $lastDate) {
-                        $articles[] = [
-                            'title' => $result['webTitle'],
-                            'sub_category_id' => $subCategory->id,
-                            'article_id' => $result['id'],
-                            'date' => Carbon::parse($result['webPublicationDate'])->format('Y-m-d H:i:s'),
-                            'content' => $result['webUrl'],
-                            'source_id' => null,
-                        ];
+                    if (!empty($articles)) {
+                        Article::insert($articles);
                     }
                 }
-
-                if (!empty($articles)) {
-                    Article::insert($articles);
-                }
             }
-        }
+        });
     }
 
 
@@ -65,47 +64,47 @@ class StoreArticleService
 
     public function newsApi()
     {
-
-        $date ='2024-01-01';
+        $date = Carbon::now()->format('Y-m-d');
         $subCategories = SubCategory::get();
 
-        foreach ($subCategories as $subCategory) {
-            $category = Category::findOrFail($subCategory->category_id);
+        DB::transaction(function () use ($subCategories, $date) {
+            foreach ($subCategories as $subCategory) {
+                $category = Category::findOrFail($subCategory->category_id);
 
-            $newsApiResponse = app(ArticleService::class)->fetchNewsArticles('https://newsapi.org/v2/everything',$subCategory->name , $date, 'b4569a220c264d548dc3479cdfb2b95f', null);
-            if ($newsApiResponse->successful()) {
-                $articles = $newsApiResponse['articles'];
+                $newsApiResponse = app(ArticleService::class)->fetchNewsArticles('https://newsapi.org/v2/everything', $subCategory->name, $date, 'b4569a220c264d548dc3479cdfb2b95f', null);
+                if ($newsApiResponse->successful()) {
+                    $articles = $newsApiResponse['articles'];
 
-                foreach ($articles as $article) {
-                    $author = Author::firstOrCreate([
-                        'name' => $article['author']
-                    ]);
+                    foreach ($articles as $article) {
+                        $author = Author::firstOrCreate([
+                            'name' => $article['author']
+                        ]);
 
-                    $source = Source::firstOrCreate([
-                        'name' => $article['source']['name']
-                    ]);
-                }
-                $lastDate = DB::table('articles')
-                ->whereNull('article_id')
-                ->max('date');
-                  if ($lastDate === null) {
-                    $lastDate = new Article();
-                    $lastDate->date = Carbon::now()->format('Y-m-d H:i:s');
-                }
-                if (Carbon::parse($article['publishedAt'])->format('Y-m-d H:i:s') >= $lastDate->date) {
-                    Article::create([
-                        'title' => $article['title'],
-                        'sub_category_id' => $subCategory->id,
-                        'article_id' => null,
-                        'date' => Carbon::parse($article['publishedAt'])->format('Y-m-d H:i:s'),
-                        'content' => $article['content'] . PHP_EOL . $article['url'],
-                        'source_id' => $source->id,
-                        'author_id' =>  $author->id
-                    ]);
+                        $source = Source::firstOrCreate([
+                            'name' => $article['source']['name']
+                        ]);
+                    }
+                    $lastDate = DB::table('articles')
+                        ->whereNull('article_id')
+                        ->max('date');
+                    if ($lastDate === null) {
+                        $lastDate = new Article();
+                        $lastDate->date = Carbon::now()->format('Y-m-d H:i:s');
+                    }
+                    if (Carbon::parse($article['publishedAt'])->format('Y-m-d H:i:s') >= $lastDate->date) {
+                        Article::create([
+                            'title' => $article['title'],
+                            'sub_category_id' => $subCategory->id,
+                            'article_id' => null,
+                            'date' => Carbon::parse($article['publishedAt'])->format('Y-m-d H:i:s'),
+                            'content' => $article['content'] . PHP_EOL . $article['url'],
+                            'source_id' => $source->id,
+                            'author_id' => $author->id
+                        ]);
+                    }
                 }
             }
-        }
+        });
     }
-
 
 }
